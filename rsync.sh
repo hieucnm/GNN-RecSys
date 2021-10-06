314ea0761c986b37ad5d9c17330bc08a3a636be9
hieucnm <hieucnm0311@gmail.com>
2021-10-06 15:41:27 +0700

    add rsync 9.17

---
+++ rsync.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+DES_PATH_1="/data/zmining/jupyter-notebook/hieucnm/graph/v5__with_edge_features/scripts/GNN-ResSys"
+MACHINE_1=zdeploy@10.50.9.13
+
+DES_PATH_2=DES_PATH_1
+MACHINE_2=zdeploy@10.50.9.13
+
+echo "---> rsync  ..."
+rsync -zavh --exclude '__pycache__' --exclude 'examples' --rsync-path="mkdir -p DES_PATH_1 && rsync" ./* $MACHINE_1:$DES_PATH_1
+rsync -zavh --exclude '__pycache__' --exclude 'examples' --rsync-path="mkdir -p $DES_PATH_2 && rsync" ./* $MACHINE_2:$DES_PATH_2
+echo "---> rsync done!"
--- rsync_9_13.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-DES_PATH="/data/zmining/jupyter-notebook/hieucnm/graph/v5__with_edge_features/scripts/GNN-ResSys"
-MACHINE=zdeploy@10.50.9.13
-
-echo "---> rsync  ..."
-rsync -zavh --exclude '__pycache__' --exclude 'examples' --rsync-path="mkdir -p $DES_PATH && rsync" ./* $MACHINE:$DES_PATH
-echo "---> rsync done!"
