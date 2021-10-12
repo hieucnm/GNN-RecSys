eafee9caa3851847af8c659d3b36cc550b49adf6
hieucnm <hieucnm0311@gmail.com>
2021-10-12 17:45:58 +0700

    minor change rsync

--- rsync.sh
+++ rsync.sh
@@ -6,7 +6,8 @@ MACHINE_1=zdeploy@10.50.9.13
 DES_PATH_2=DES_PATH_1
 MACHINE_2=zdeploy@10.50.9.17
 
+
 echo "---> rsync  ..."
-rsync -zavh --exclude '__pycache__' --exclude 'examples' --rsync-path="mkdir -p DES_PATH_1 && rsync" ./* $MACHINE_1:$DES_PATH_1
+rsync -zavh --exclude '__pycache__' --exclude 'base' --rsync-path="mkdir -p DES_PATH_1 && rsync" ./* $MACHINE_1:$DES_PATH_1
 # rsync -zavh --exclude '__pycache__' --exclude 'examples' --rsync-path="mkdir -p $DES_PATH_2 && rsync" ./* $MACHINE_2:$DES_PATH_2
 echo "---> rsync done!"
